fa3719f5d4f5c41d28d2b33bdb3b5d7114167b45
Hugo Gallardo <hugo.gif@gmail.com>
2023-11-13 13:03:11 -0300

    change from dnf to yum for installing services

--- freeipa_set_commands.sh
+++ freeipa_set_commands.sh
@@ -2,6 +2,13 @@
 # # if Red Hat IPA is used as the KDC
 # yum install freeipa-client -y
 
+#Remove IPV6 from 
+source /etc/profile
+echo net.ipv6.conf.all.disable_ipv6 = 1 >> /etc/sysctl.conf
+echo net.ipv6.conf.default.disable_ipv6 = 1 >> /etc/sysctl.conf
+echo net.ipv6.conf.lo.disable_ipv6 = 1 >> /etc/sysctl.conf
+sysctl -p
+
 pass_ipa="B@dPassw0rd!"
 domain="laboratory.cloudera.net"
 realm="LABORATORY.CLOUDERA.NET"
@@ -10,8 +17,8 @@ host_ip=$(hostname -i)
 host_name="$(hostname).$domain"
 
 sudo /usr/bin/hostnamectl set-hostname ${host_name}
-sudo dnf install freeipa-server -y
-sudo dnf install -y ipa-server-dns bind-dyndb-ldap
+sudo yum install freeipa-server -y
+sudo yum install -y ipa-server-dns bind-dyndb-ldap
 
 sudo sed -i "s/search/#search/g" /etc/resolv.conf
 sudo sed -i "s/nameserver/#nameserver/g" /etc/resolv.conf
