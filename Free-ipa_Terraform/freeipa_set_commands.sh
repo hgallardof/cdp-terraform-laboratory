e8fb00179eaa12786786788ec5b79b1be7150cc9
Hugo Gallardo <hugo.gif@gmail.com>
2023-12-07 11:02:20 -0300

    set new structure

--- Free-ipa_Terraform/freeipa_set_commands.sh
+++ Free-ipa_Terraform/freeipa_set_commands.sh
@@ -1,13 +1,15 @@
 pass_ipa="Cloudera.2023"
-domain="laboratory.cloudera.net"
-realm="LABORATORY.CLOUDERA.NET"
-reverse_zone="1.0.10"
+domain="internal.cloudapp.net"
+realm="INTERNAL.CLOUDAPP.NET"
+reverse_zone="0.0.10"
 
+sudo hostname $(hostname).$domain
 sudo yum install freeipa-server -y
 sudo yum install -y ipa-server-dns bind-dyndb-ldap
-
+# sudo sed -i "s/internal.cloudapp.net/laboratory.cloudera.net/g" /etc/hostname;hostname; sudo ipa-client-install
 # sudo sed -i "s/search/#search/g" /etc/resolv.conf
-# sudo sed -i "s/nameserver/#nameserver/g" /etc/resolv.conf
+sudo sed -i "s/nameserver/#nameserver/g" /etc/resolv.conf
+echo "nameserver $(hostname -i)" | sudo tee -a /etc/resolv.conf
 
 # echo "search laboratory.cloudera.net
 # nameserver $(hostname -i)" | sudo tee -a /etc/resolv.conf
